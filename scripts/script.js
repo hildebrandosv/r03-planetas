c536919f88c85d1cdea207af0575752689862b7c
hildebrandosv <hildebrandosv@gmail.com>
2021-12-14 16:11:40 -0500

    Carpetas del JavaScript

---
+++ scripts/script.js
@@ -0,0 +1,23 @@
+//╔════════════════════════════════════════════════╗
+//║             MAIN JAVASCRIPT SCRIPT             ║
+//╚════════════════════════════════════════════════╝
+// Importar el archivo de datos 
+// Functions import
+import { fnSetValueInput, fnUpdateCartCounter } from "../scripts/general.js";
+// *** Constant definition ***
+//
+// *** Variables definition ***
+//
+// *********************************************
+// ***           BEGIN MAIN MODULE           ***
+// *********************************************
+//
+//
+// *********************************************
+// ***            END MAIN MODULE            ***
+// *********************************************
+//╔════════════════════════════════════════════════╗
+//║             FUNCTION DEFINITION                ║
+//╚════════════════════════════════════════════════╝
+function al(aa) { alert("Aquí voy... ", aa); }
+function cl(aa) { console.log("Aquí voy... ", aa); }
