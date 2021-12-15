ed1ed1f486ec6558428ca8f7aab17e657bb7e987
hildebrandosv <hildebrandosv@gmail.com>
2021-12-15 07:41:00 -0500

    Agregar información de repaso JS

--- scripts/script.js
+++ scripts/script.js
@@ -1,17 +1,63 @@
 //╔════════════════════════════════════════════════╗
 //║             MAIN JAVASCRIPT SCRIPT             ║
 //╚════════════════════════════════════════════════╝
-// Importar el archivo de datos 
+// Importar el archivo de datos
+//
 // Functions import
-import { fnSetValueInput, fnUpdateCartCounter } from "../scripts/general.js";
+//
 // *** Constant definition ***
+const tamagno = []
 //
 // *** Variables definition ***
+let nombrePlanetas = Array('Tierra', 'Marte')
+let distancia = [6, 3, '67']
 //
 // *********************************************
 // ***           BEGIN MAIN MODULE           ***
 // *********************************************
 //
+console.log(nombrePlanetas)
+console.log(distancia)
+console.log(tamagno)
+console.log('Tipos de formas de recorrer un array')
+console.log('1. Con ciclo while')
+let i = 0
+while (i <= nombrePlanetas.length - 1) {
+   console.log(i + " --- " + nombrePlanetas[i])
+   i += 1
+}
+console.log('2. Ciclo for')
+for (let index = 0; index < nombrePlanetas.length; index++) {
+   console.log(index + " --- " + nombrePlanetas[index])
+}
+console.log('3. Ciclo for In')
+for (const key in nombrePlanetas) {
+   console.log(nombrePlanetas[key])
+}
+console.log('4. Ciclo for Of')
+for (const planeta of nombrePlanetas) {
+   console.log(planeta)
+}
+console.log('Buscar distancia con ==')
+distancia = [5, 3, '67']
+for (const key in distancia) {
+   if (distancia[key] ==  '5') {
+      console.log('Se encontró')
+   } else {
+      console.log('No se encontró')
+   }
+}
+console.log('Buscar distancia con operador estricto ===')
+distancia = [5, 3, '67']
+for (const key in distancia) {
+   if (distancia[key] ===  '5') {
+      console.log('Se encontró')
+   } else {
+      console.log('No se encontró')
+   }
+}
+
+
 //
 // *********************************************
 // ***            END MAIN MODULE            ***
