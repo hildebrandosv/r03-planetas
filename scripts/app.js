dd16b72fc970c845f1fe0e0a5c5a11f9fc8c3ff0
hildebrandosv <hildebrandosv@gmail.com>
2021-12-16 18:15:59 -0500

    Implementación del ordenamento de la información en la tabla por nombre, distancia y tamaño

--- scripts/app.js
+++ scripts/app.js
@@ -18,6 +18,20 @@ let tbPlanetTmp = [];    // To storage the planets in other order
 //
 const elPlanetsBodyTable = document.getElementById('idBodyTable');
 fnFillTable(elPlanetsBodyTable, tbPlanets, "DEFAULT");
+// Listeners the event click in the three choices of tre order dropdown
+const elIdByName = document.getElementById('idByName').addEventListener('click',
+   e => {
+      fnFillTable(elPlanetsBodyTable, tbPlanets, "NAME");
+   })
+const elIdByDistance = document.getElementById('idByDistance').addEventListener('click',
+   e => {
+      fnFillTable(elPlanetsBodyTable, tbPlanets, "DISTANCE");
+   })
+const elIdBySize = document.getElementById('idBySize').addEventListener('click',
+   e => {
+      fnFillTable(elPlanetsBodyTable, tbPlanets, "SIZE");
+   })
+console.log(Number("1.23"))
 
 //
 // *********************************************
@@ -28,20 +42,59 @@ function cl(aa) { console.log("Aquí voy... ", aa); }
 //╔════════════════════════════════════════════════╗
 //║             FUNCTION DEFINITION                ║
 //╚════════════════════════════════════════════════╝
-function fnFillTable(elementBodyId, aTheArray, sTheOrder) {
-   // To order the array according to user chooses
-   //
-   // To fill the table and show it
-   let elTableRows= '';
+function fnFillTable(elementBodyId, aTheArray, sTheOrder)
+// Displays the info in the table, according to an order
+{
+   console.log(aTheArray)
+   console.log(sTheOrder)
+   // First orders the array according to user chooses and according to "sTheOrder" parameter
+   switch (sTheOrder.toUpperCase()) {
+      case 'NAME':
+         aTheArray.sort((a, b) => {
+            if (a.name > b.name) {
+               return 1;
+            }
+            if (a.name < b.name) {
+               return -1;
+            }
+            // a must be equal to b
+            return 0;
+         });
+         break;
+      case 'SIZE':
+         aTheArray.sort((a, b) => {
+            if (Number(a.size) > Number(b.size)) {
+               return 1;
+            }
+            if (Number(a.size) < Number(b.size)) {
+               return -1;
+            }
+            // a must be equal to b
+            return 0;
+         });
+         break;
+      default:
+         aTheArray.sort((a, b) => {
+            if (Number(a.distance) > Number(b.distance)) {
+               return 1;
+            }
+            if (Number(a.distance) < Number(b.distance)) {
+               return -1;
+            }
+            // a must be equal to b
+            return 0;
+         });
+         break;
+   }
+   // To fill the table and show it ordered
+   let elTableRows = '';
    aTheArray.forEach((element, index) => {
       elTableRows += `<tr>
-                        <td scope="row">${index+1}</td>
+                        <td scope="row">${index + 1}</td>
                         <td>${element.name}</td>
                         <td>${element.distance}</td>
                         <td>${element.size}</td>
                      </tr>`
-
-      console.log(elTableRows);
    });
-   elementBodyId.innerHTML= elTableRows;
+   elementBodyId.innerHTML = elTableRows;
 }
